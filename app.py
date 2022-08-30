7f262ad5af616f17aa0a58223c2f41467a750d8f
Scott Goodell <scott@hipcamp.com>
2022-08-29 18:34:08 -0700

    rm wsgi use app

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from apscheduler.schedulers.background import BackgroundScheduler
-from .game_final_updater import GameFinalUpdater
-from .available_chart_players import available_chart_players
-from .game_shot_plot import GameShotPlot
-# from .tweeter import Tweeter
+
+from src.game_final_updater import GameFinalUpdater
+from src.available_chart_players import available_chart_players
+from src.game_shot_plot import GameShotPlot
+from services.tweeter import Tweeter
 
 
 scheduler = BackgroundScheduler(daemon=True)
@@ -24,9 +25,11 @@ def poll_games():
           print(f"Generating chart for player: {player['player_id']} team: {player['team_id']}..")
           img_link = build_chart(team_id = player["team_id"], player_id = player["player_id"], game_id = game["game_id"])
           
+          # TODO: We should skip sending tweet if the above chart building returns None
           print(f"Finished generating chart with link: {img_link}")
           print(f"Sending tweet with newly created img: {img_link}")
           # send_tweet(media_link = img_link)
+          # TODO: img file cleanup here after sending the tweet? At this point we should have backed it up to GCP
   else:
     print("No new games recently finished")
 
@@ -36,9 +39,9 @@ def build_chart(team_id, player_id, game_id):
 
   return img_link
   
-# def send_tweet(media_link, account = "foobar"):
-#   tweeter = Tweeter()
-#   tweeter.send_tweet(media_link)
+def send_tweet(media_link, account = "foobar"):
+  tweeter = Tweeter()
+  tweeter.send_tweet(media_link)
 
 @app.route("/")
 def hello_hunty():
@@ -52,7 +55,9 @@ def manual_poll():
     "status": 200
   }
 
-scheduler.add_job(poll_games, 'interval', seconds=5)
-# scheduler.start()
-  
-# class to run through static files and check to see if tweet should be sent
+scheduler.add_job(poll_games, 'interval', seconds=10)
+scheduler.start()
+
+
+if __name__ == "__main__":
+  app.run(debug = False, port = 1234)
