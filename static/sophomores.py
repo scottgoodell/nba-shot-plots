12fec0bf66687b01607336fcda85ed594550b1f4
Scott Goodell <scott@hipcamp.com>
2022-10-08 13:18:42 -0700

    add sophs

--- static/sophomores.py
+++ static/sophomores.py
@@ -22,5 +22,45 @@ sophomores = [
     "draft_year": 2021,
     "draft_pick": 3,
     "active": True
+  },
+  {
+    "id": 1630567,
+    "slug": "scottie-barnes",
+    "name": "Scottie Barnes",
+    "draft_year": 2021,
+    "draft_pick": 4,
+    "active": True
+  },
+  {
+    "id": 1630591,
+    "slug": "jalen-suggs",
+    "name": "Jalen Suggs",
+    "draft_year": 2021,
+    "draft_pick": 5,
+    "active": True
+  },
+  {
+    "id": 1630581,
+    "slug": "josh-giddey",
+    "name": "Josh Giddey",
+    "draft_year": 2021,
+    "draft_pick": 6,
+    "active": True
+  },
+  {
+    "id": 1630228,
+    "slug": "jonathan-kuminga",
+    "name": "Jonathan Kuminga",
+    "draft_year": 2021,
+    "draft_pick": 7,
+    "active": True
+  },
+  {
+    "id": 1630532,
+    "slug": "franz-wagner",
+    "name": "Franz Wagner",
+    "draft_year": 2021,
+    "draft_pick": 8,
+    "active": True
   }
 ]
