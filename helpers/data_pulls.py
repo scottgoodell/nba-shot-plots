96d38d6d88067dc011c450c2499fa68696284510
Scott Goodell <scott@hipcamp.com>
2022-11-10 14:36:08 -0800

    error handle shot data

--- helpers/data_pulls.py
+++ helpers/data_pulls.py
@@ -14,18 +14,22 @@ def player_game_shot_data(
   season_part
 ):
   delay_between_1_and_2_secs()
-  shot_json = shotchartdetail.ShotChartDetail(
-    team_id = team_id,
-    player_id = player_id,
-    game_id_nullable = game_id,
-    context_measure_simple = shot_type,
-    season_nullable = season,
-    season_type_all_star = season_part
-  )
-  shot_data = json.loads(shot_json.get_json())
-  shot_values = shot_data["resultSets"][0]["rowSet"]
+  try:
+    shot_json = shotchartdetail.ShotChartDetail(
+      team_id = team_id,
+      player_id = player_id,
+      game_id_nullable = game_id,
+      context_measure_simple = shot_type,
+      season_nullable = season,
+      season_type_all_star = season_part
+    )
+    shot_data = json.loads(shot_json.get_json())
+    shot_values = shot_data["resultSets"][0]["rowSet"]
 
-  return shot_values
+    return shot_values
+  except Exception as e:
+    print(f"Game shot data failed: {e}")
+    return []
 
 def get_game_boxscore(game_id):
   delay_between_1_and_2_secs()
